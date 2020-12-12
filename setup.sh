e7a13296c8796f4e5f6693e145ae367504e924c9
Bangbadruyt <75634180+Bangbadruyt@users.noreply.github.com>
2020-12-12 16:24:32 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,56 +8,56 @@ pip install requests
 clear
 echo
 echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
-echo "Ucapkan CYBER EROR Ganteng 3X Biar Work Xixixi:v"
+echo "Ucapkan CYBER EROR 404 Ganteng 3X Biar Work Xixixi:v"
 echo
 pip install mechanize
 clear
 echo
 echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
-echo "Ucapkan CYBER EROR Ganteng 3X Biar Work Xixixi:v"
+echo "Ucapkan CYBER EROR 404 Ganteng 3X Biar Work Xixixi:v"
 echo
 pip install requests bs4
 clear
 echo
 echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
-echo "Ucapkan CYBER EROR Ganteng 3X Biar Work Xixixi:v"
+echo "Ucapkan CYBER EROR 404 Ganteng 3X Biar Work Xixixi:v"
 echo
 pkg install figlet
 clear
 echo
 echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
-echo "Ucapkan CYBER EROR Ganteng 3X Biar Work Xixixi:v"
+echo "Ucapkan CYBER EROR 404Ganteng 3X Biar Work Xixixi:v"
 echo
 pip2 install requests
 clear
 echo
 echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
-echo "Ucapkan CYBER EROR Ganteng 3X Biar Work Xixixi:v"
+echo "Ucapkan CYBER EROR 404 Ganteng 3X Biar Work Xixixi:v"
 echo
 pip2 install tqdm
 clear
 echo
 echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
-echo "Ucapkan CYBER EROR Ganteng 3X Biar Work Xixixi:v"
+echo "Ucapkan CYBER EROR 404 Ganteng 3X Biar Work Xixixi:v"
 echo
 pip2 install mechanize
 clear
 echo
 echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
-echo "Ucapkan CYBER EROR Ganteng 3X Biar Work Xixixi:v"
+echo "Ucapkan CYBER EROR 404 Ganteng 3X Biar Work Xixixi:v"
 echo
 pkg install ruby -y
 clear
 echo
 echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
-echo "Ucapkan CYBER EROR Ganteng 3X Biar Work Xixixi:v"
+echo "Ucapkan CYBER EROR 404 Ganteng 3X Biar Work Xixixi:v"
 echo
 gem install lolcat
 clear
-figlet CYBER EROR | lolcat
+figlet CYBER EROR 404 | lolcat
 sleep 2
 clear
-figlet CYBER EROR | lolcat
+figlet CYBER EROR 404 | lolcat
 sleep 2
 clear
 echo
